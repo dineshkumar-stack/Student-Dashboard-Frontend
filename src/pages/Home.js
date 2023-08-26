4f3968f06b49bdf527df501af673da9e6010db08
dineshkumar-stack <stardinesh4@gmail.com>
2023-08-27 00:26:53 +0530

    some changes added and till codekata completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,19 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GridStack } from 'gridstack';
 import 'gridstack/dist/gridstack.css';
-import TaskPage from '../pages/TaskPage';
 import PieChart from "./PieChart";
 import BarChart from "./BarChart";
 import ChartPage from '../pages/ChartPage';
 import Github from "../components/Github";
+import AdditionalClass from "./AddClass";
+import AllDetailsInOne from "./Task_AddClass";
+import CodeKataDetails from "./CodekataPage";
 
 function Home() {
 
 
-  
-  useEffect(() => {    
+
+  useEffect(() => {
     var options = { // put in gridstack options here
       disableOneColumnMode: true, // for jfiddle small window size
       float: false
@@ -21,31 +23,38 @@ function Home() {
 
     grid.enableMove(true);
     grid.enableResize(true);
-    
+
   });
   return (
     <Router>
       <div className="home">
         <div className="container-lg">
-          <h2>Welcome to the Student Dashboard</h2>
         </div>
         <div className="grid-stack">
-        <div className="grid-stack-item border-dark"  gs-x="0"  gs-w="10" gs-h="3">
-            <div className="grid-stack-item-content"><TaskPage /></div>
+          <div className="grid-stack-item border-dark" gs-x="0" gs-w="4" gs-h="5">
+            <div className="grid-stack-item-content"><AllDetailsInOne /></div>
           </div>
-          <div className="grid-stack-item border-dark"  gs-x="1"  gs-w="2" gs-h="3">
-            <div className="grid-stack-item-content"><PieChart /></div>
-          </div>
-
-          <div className="grid-stack-item border-dark" gs-x="2"  gs-w="3" gs-h="2">
+          <div className="grid-stack-item border-dark" gs-x="2" gs-w="3" gs-h="2">
             <div className="grid-stack-item-content"><BarChart /></div>
           </div>
-          <div className="grid-stack-item border-dark" gs-x="3"  gs-w="3" gs-h="2">
-          <div className="grid-stack-item-content"><ChartPage /></div>
-        </div>
-        <div className="grid-stack-item border-dark" gs-x="4"  gs-w="6" gs-h="2">
-          <div className="grid-stack-item-content"><Github /></div>
-        </div>
+          <div className="grid-stack-item border-dark" gs-x="3" gs-w="3" gs-h="2">
+            <div className="grid-stack-item-content"><ChartPage /></div>
+          </div>
+          <div className="grid-stack-item border-dark" gs-x="1" gs-w="2" gs-h="3">
+            <div className="grid-stack-item-content"><PieChart /></div>
+          </div>
+          <div className="grid-stack-item border-dark" gs-x="4" gs-w="6" gs-h="2">
+            <div className="grid-stack-item-content"><Github /></div>
+          </div>
+          <div className="grid-stack-item border-dark" gs-x="5" gs-w="2" gs-h="5">
+            <div className="grid-stack-item-content"><AdditionalClass /></div>
+          </div>
+          <div className="grid-stack-item border-dark" gs-x="3" gs-w="3" gs-h="2">
+            <div className="grid-stack-item-content"><CodeKataDetails /></div>
+          </div>
+          {/* <div className="grid-stack-item border-dark" gs-x="4" gs-w="6" gs-h="2">
+            <div className="grid-stack-item-content"><Road /></div>
+          </div> */}
         </div>
       </div>
     </Router>
