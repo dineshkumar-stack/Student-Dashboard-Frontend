cbc87bd42bad9c7cfe6b722083abee0f142b2ea6
dineshkumar-stack <stardinesh4@gmail.com>
2023-08-30 18:43:54 +0530

    task submit completed

--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -1,17 +1,65 @@
-import Button from "react-bootstrap/Button";
-import React, { useState, useEffect } from 'react';
-
+import { Button, Modal, Form } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import Accordion from "react-bootstrap/Accordion";
+import dateFormat from 'dateformat';
 
 function TaskPage() {
-
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    fetch('https://student-dashboard-be.onrender.com/api/tasks')
-      .then(response => response.json())
-      .then(data => setTasks(data))
-      .catch(error => console.error('Error fetching tasks:', error));
+    fetch("https://student-dashboard-be.onrender.com/api/taskbarstatus")
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.error("Error fetching tasks:", error));
   }, []);
+
+  const [showModal, setShowModal] = useState(false);
+  const [taskTitle, setTaskTitle] = useState("");
+  const [FELink, setFELink] = useState("");
+  const [SNo, setSNo] = useState("");
+  const [BELink, setBELink] = useState("");
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => {setShowModal(false);
+    resetForm()
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch(
+        "https://student-dashboard-be.onrender.com/api/taskbarstatus",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: taskTitle,
+            SNo: SNo,
+            FrontEndLink: FELink,
+            BackEndLink: BELink,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        console.log("Task submitted successfully");
+
+        handleCloseModal();
+      } else {
+        console.error("Error submitting task");
+      }
+    } catch (error) {
+      console.error("Error submitting task:", error);
+    }
+  };
+
+  const resetForm = () => {
+    setTaskTitle("");
+    setSNo("");
+    setFELink("");
+    setBELink("");
+  };
 
   return (
     <div className="task-page">
@@ -20,36 +68,88 @@ function TaskPage() {
           <tr>
             <td colSpan="5">
               <h4>Task Page</h4>
+              <Button variant="primary" onClick={handleShowModal}>
+                Task Submits
+              </Button>
             </td>
-          </tr>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Mark</th>
-            <th>Link</th>
-            <th>Recordings</th>
           </tr>
         </thead>
         <tbody>
           {tasks.map((task) => (
-            <tr key={task.id}>
-              <td>{task.title}</td>
-              <td>{task.description}</td>
-              <td>{task.mark}</td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter link"
-                />
-              </td>
-              <td>
-                <Button variant="outline-info" href={task.link} >Play Recordings</Button>{" "}
-              </td>
-            </tr>
+            <Accordion defaultActiveKey="1">
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>{task.title}</Accordion.Header><span>Submitted on: {dateFormat(task.timeStamp,"hh:mm | dddd | mmmm dS | yyyy")}</span>
+                <Accordion.Body>
+                  <table>
+                    <tr>
+                      <td>
+                        <strong>Front End Link : </strong>
+                        <a href={task.FrontEndLink}>{task.FrontEndLink}</a>
+                        <br />
+                        <strong>Back End Link : </strong>
+                        <a href={task.BackEndLink}>{task.BackEndLink}</a>
+                      </td>
+                    </tr>
+                  </table>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           ))}
         </tbody>
       </table>
+
+      <Modal size="lg" show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="SNo">
+              <Form.Control
+                type="Number"
+                placeholder="S.NO"
+                value={SNo}
+                onChange={(e) => setSNo(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="taskTitle">
+              <Form.Control
+                type="text"
+                placeholder="Enter task title"
+                value={taskTitle}
+                onChange={(e) => setTaskTitle(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="FELink">
+              <Form.Control
+                type="text"
+                placeholder="Front-end Source code"
+                value={FELink}
+                onChange={(e) => setFELink(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="BELink">
+              <Form.Control
+                type="text"
+                placeholder="Back-end Source code"
+                value={BELink}
+                onChange={(e) => setBELink(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
